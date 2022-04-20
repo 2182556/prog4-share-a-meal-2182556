cb136b6625317c1dd5301df9c6628b7b97bccd7c
2182556 <je.boellaard@student.avans.nl>
2022-04-20 18:07:17 +0200

    removed comment

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
-//testing workflow
 
 app.use(bodyParser.json());
 
