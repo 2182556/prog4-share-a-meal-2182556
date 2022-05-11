fd1058eaecec2ae8dc251fdc5d76a834d851eeb3
2182556 <je.boellaard@student.avans.nl>
2022-05-11 20:50:30 +0200

    changed local test database

--- test/integration/user.controller.test.js
+++ test/integration/user.controller.test.js
@@ -2,15 +2,52 @@ const chai = require('chai')
 const chaiHttp = require('chai-http')
 const server = require('../../index')
 
+process.env.DB_DATABASE = process.env.DB_DATABASE || 'share-a-meal-testdb'
+require('dotenv').config()
+const dbconnection = require('../../database/dbconnection')
+
 chai.should()
 chai.use(chaiHttp)
+
+/**
+ * Db queries to clear and fill the test database before each test.
+ */
+const CLEAR_MEAL_TABLE = 'DELETE IGNORE FROM `meal`;'
+const CLEAR_PARTICIPANTS_TABLE = 'DELETE IGNORE FROM `meal_participants_user`;'
+const CLEAR_USERS_TABLE = 'DELETE IGNORE FROM `user`;'
+const CLEAR_DB = CLEAR_MEAL_TABLE + CLEAR_PARTICIPANTS_TABLE + CLEAR_USERS_TABLE
+
+/**
+ * Voeg een user toe aan de database. Deze user heeft id 1.
+ * Deze id kun je als foreign key gebruiken in de andere queries, bv insert studenthomes.
+ */
+const INSERT_USER =
+  'INSERT INTO `user` (`id`, `firstName`, `lastName`, `emailAdress`, `password`, `street`, `city` ) VALUES' +
+  '(1, "first", "last", "name@server.nl", "secret", "street", "city")' +
+  '(2, "Davide", "Ambesi", "d.ambesi@avans.nl", "secret", "street", "city");'
+
+/**
+ * Query om twee meals toe te voegen. Let op de UserId, die moet matchen
+ * met de user die je ook toevoegt.
+ */
+const INSERT_MEALS =
+  'INSERT INTO `meal` (`id`, `name`, `description`, `imageUrl`, `dateTime`, `maxAmountOfParticipants`, `price`, `cookId`) VALUES' +
+  "(1, 'Meal A', 'description', 'image url', NOW(), 5, 6.50, 1)," +
+  "(2, 'Meal B', 'description', 'image url', NOW(), 5, 6.50, 1);"
 
 describe('Manage users', () => {
   //Use case 201
   describe('UC-201 Register /api/user', () => {
-    beforeEach((done) => {
+    before((done) => {
+      console.log(
+        'before: hier zorg je eventueel dat de precondities correct zijn'
+      )
+      console.log('before done')
       done()
     })
+    // beforeEach((done) => {
+    //   done()
+    // })
     //add it.only if you only want to test one several times (or it.skip)
     it('TC-201-1 When a required input is missing, a validation error should be returned', (done) => {
       chai
